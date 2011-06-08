434e04a88e87c8c2d69927d6639cf876e297f84f
Will Brown <will.h.brown@gmail.com>
2011-06-08 08:31:08 -0400

    Added interactive mode to lispy (-i flag)

--- src/lispy.py
+++ src/lispy.py
@@ -10,6 +10,16 @@ import sys
 lexer = lispylex.lexer()
 parser = lispyacc.parser()
 
-s = sys.stdin.read()
-result = parser.parse(s)
-result.evaluate(lispyenv.LispyEnv())
+if '-i' in sys.argv:
+    while True:
+        try:
+            line = raw_input('> ')
+        except EOFError:
+            break
+        if line == '(quit)':
+            break
+        parser.parse(line).evaluate(lispyenv.LispyEnv())
+else:
+    s = sys.stdin.read()
+    result = parser.parse(s)
+    result.evaluate(lispyenv.LispyEnv())
